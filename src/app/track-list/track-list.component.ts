5b02e1d80d2a918b3959d1e8e91843b015ed23c0
Magdalena Kochman <magdakochman7@gmail.com>
2023-05-09 22:03:13 +0200

    refactor track-list component

--- src/app/track-list/track-list.component.ts
+++ src/app/track-list/track-list.component.ts
@@ -1,56 +1,35 @@
 import { TrackService } from './../services/track.service';
 import { PlayerService } from './../services/player.service';
 import { Item } from './../interfaces/track';
-import { debounceTime, fromEvent, map, Observable, Subscription } from 'rxjs';
-import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, EventEmitter, Input, Output, Signal } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-track-list',
   templateUrl: './track-list.component.html',
   styleUrls: ['./track-list.component.scss'],
 })
-export class TrackListComponent implements OnInit, OnDestroy {
-  @Input() tracksList = new Observable<Item[]>();
-
+export class TrackListComponent {
+  @Input() tracksList!: Signal<Item[]>;
   @Input() total = 0;
-
   @Output() getNextPage = new EventEmitter<PageEvent>();
-
-  currentPage = 0;
-
+  currentPage = toSignal(this.trackService.currentPage);
   pageSize = this.trackService.pageSize;
 
-  tracks: Item[] = [];
-
-  subscriptions = new Subscription();
-
-  constructor(private playerService: PlayerService, private trackService: TrackService) {
-  }
-
-  ngOnInit(): void {
-    this.subscriptions.add(this.tracksList.subscribe((val) => {
-      this.tracks = val
-    }))
-
-    this.trackService.currentPage.subscribe((val) => {
-      this.currentPage = val
-    })
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.unsubscribe();
-    this.tracks = []
-    this.currentPage = 0;
-  }
+  constructor(
+    private playerService: PlayerService,
+    private trackService: TrackService
+  ) {}
 
   play(index: number) {
-    const queue = this.tracks.map((item) => item.track.uri)
-    this.playerService.queue.next({ queue, index })
+    const queue = this.tracksList()?.map((item) => item.track.uri);
+    if (queue) this.playerService.queue.next({ queue, index });
   }
 
   handlePageEvent(page: PageEvent) {
-    this.trackService.currentPage.next(page.pageIndex)
+    this.trackService.currentPage.next(page.pageIndex);
     this.getNextPage.emit(page);
   }
 }
