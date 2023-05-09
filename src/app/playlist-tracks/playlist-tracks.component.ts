ee8e0303738393d08b5d7245eddfe75ad1316114
Magdalena Kochman <magdakochman7@gmail.com>
2023-05-09 22:03:41 +0200

    refactor savedt component

--- src/app/playlist-tracks/playlist-tracks.component.ts
+++ src/app/playlist-tracks/playlist-tracks.component.ts
@@ -1,43 +1,62 @@
 import { TrackService } from './../services/track.service';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { EMPTY, Observable, filter, forkJoin, map, switchMap, tap } from 'rxjs';
+import { Component, OnInit, Signal } from '@angular/core';
+import {
+  EMPTY,
+  Observable,
+  filter,
+  forkJoin,
+  map,
+  switchMap,
+  tap,
+  zip,
+} from 'rxjs';
 import { Item } from '../interfaces/track';
 import { PageEvent } from '@angular/material/paginator';
-import {takeUntilDestroyed, toSignal} from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-playlist-tracks',
   templateUrl: './playlist-tracks.component.html',
-  styleUrls: ['./playlist-tracks.component.scss']
+  styleUrls: ['./playlist-tracks.component.scss'],
 })
 export class PlaylistTracksComponent implements OnInit {
-  tracks = this.trackService.getPlaylistTracks();
+  tracks: Signal<Item[]> = toSignal(this.trackService.getPlaylistTracks(), {
+    initialValue: [],
+  });
   playlistId = '';
-  title = toSignal(this.trackService.playlisInfo.pipe(map((val) => val.name)))
-  total = toSignal(this.trackService.totalTracks, {requireSync: true})
+  title = toSignal(
+    this.trackService.getPlaylistInfo().pipe(map((val) => val.name))
+  );
+  total = toSignal(this.trackService.totalTracks, { requireSync: true });
   playlistData$ = this.route.paramMap.pipe(
     takeUntilDestroyed(),
-    switchMap((paramMap) => {
-      const playlistId = paramMap.get('id');
-      if (playlistId) {
-        this.playlistId = playlistId;
-        this.trackService.currentPage.next(0);
-        this.trackService.retrivePlaylistTracks(playlistId);
-        this.trackService.retrivePlaylist(playlistId);
-      }
+    map((paramMap) => paramMap.get('id')),
+    switchMap((playlistId) => {
+      if (!playlistId) return EMPTY;
 
-      return EMPTY
+      this.playlistId = playlistId;
+      this.trackService.currentPage.next(0);
+      return zip(
+        this.trackService.retrivePlaylistTracks(playlistId),
+        this.trackService.retrivePlaylist(playlistId)
+      );
     })
   );
 
-  constructor(private route: ActivatedRoute, private trackService: TrackService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private trackService: TrackService
+  ) {}
 
   ngOnInit(): void {
-    this.playlistData$.subscribe()
+    this.playlistData$.subscribe();
   }
 
   getNextPage(page: PageEvent) {
-    this.trackService.retrivePlaylistTracks(this.playlistId, page.pageSize * page.pageIndex);
+    this.trackService.retrivePlaylistTracks(
+      this.playlistId,
+      page.pageSize * page.pageIndex
+    );
   }
 }
