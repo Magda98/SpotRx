32ce6e4fbab43c6a7d4d4937b9393b2d1409de4d
Magda98 <magdakochman7@gmail.com>
2024-02-16 09:28:23 +0100

    fix skeleton loading

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,13 +4,19 @@ import { Component, inject } from '@angular/core';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [RouterModule, CommonModule, NgOptimizedImage],
+  imports: [
+    RouterModule,
+    CommonModule,
+    NgOptimizedImage,
+    NgxSkeletonLoaderModule,
+  ],
 })
 export class HomeComponent {
   private trackService = inject(TrackService);
