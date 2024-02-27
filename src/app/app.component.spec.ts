759a66e232491d4cd10c956499f3408d4c1ee6ca
Magdalena Bober <33430525+Magda98@users.noreply.github.com>
2024-02-27 07:22:24 +0000

    add tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,7 +5,7 @@ import {
   QueryClient,
   provideAngularQuery,
 } from '@tanstack/angular-query-experimental';
-import { RouterModule } from '@angular/router';
+import { RouterModule, provideRouter } from '@angular/router';
 import { BehaviorSubject, ReplaySubject, of } from 'rxjs';
 import { AuthData } from './interfaces/authData';
 import { NavComponent } from './nav/nav.component';
@@ -13,10 +13,11 @@ import { IconComponent } from './icon/icon.component';
 import { PlayerComponent } from './player/player.component';
 import { AngularQueryDevtools } from '@tanstack/angular-query-devtools-experimental';
 import { CommonModule } from '@angular/common';
-import { render, screen } from '@testing-library/angular';
+import { fireEvent, render, screen } from '@testing-library/angular';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MockBackendInterceptor } from './interceptors/mock-backend.interceptor';
+import { routes } from './app-routes';
 
 describe('AppComponent', () => {
   const mockAuthService = createMockWithValues(AuthService, {
@@ -76,5 +77,60 @@ describe('AppComponent', () => {
     });
     component.detectChanges();
     expect(screen.getByRole('main')).toBeInTheDocument();
+  });
+
+  test('should navigate', async () => {
+    mockAuthService.loggedIn.next(true);
+    const component = await render(AppComponent, {
+      componentProviders: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
+      componentImports: [
+        RouterModule,
+        NavComponent,
+        IconComponent,
+        PlayerComponent,
+        AngularQueryDevtools,
+        CommonModule,
+      ],
+      imports: [HttpClientTestingModule, RouterModule],
+      providers: [
+        provideRouter(routes),
+        provideAngularQuery(new QueryClient()),
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: MockBackendInterceptor,
+          multi: true,
+        },
+      ],
+    });
+    jest.useFakeTimers();
+    component.detectChanges();
+    expect(screen.queryByText(/Saved tracks/i)).not.toBeInTheDocument();
+
+    await fireEvent.click(screen.getByRole('link', { name: /Saved/i }));
+    expect(
+      await screen.findByRole('heading', { name: /Saved tracks/i })
+    ).toBeInTheDocument();
+
+    await fireEvent.click(screen.getByRole('link', { name: /Home/i }));
+    expect(
+      await screen.findByRole('heading', { name: /Home/i })
+    ).toBeInTheDocument();
+
+    await fireEvent.click(screen.getByRole('link', { name: /Home/i }));
+    expect(
+      await screen.findByRole('heading', { name: /Home/i })
+    ).toBeInTheDocument();
+
+    jest.advanceTimersByTime(300);
+    component.detectChanges();
+    await fireEvent.click(screen.getByRole('link', { name: /Dua Lipa/i }));
+    expect(
+      await screen.findByRole('heading', { name: /Dua Lipa/i })
+    ).toBeInTheDocument();
   });
 });
--- src/app/interceptors/mock-backend.interceptor.ts
+++ src/app/interceptors/mock-backend.interceptor.ts
@@ -11,7 +11,7 @@ import {
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
-import { PlaylistResponse } from '../interfaces/playlist';
+import { Playlist, PlaylistResponse } from '../interfaces/playlist';
 
 @Injectable()
 export class MockBackendInterceptor implements HttpInterceptor {
@@ -125,6 +125,91 @@ export class MockBackendInterceptor implements HttpInterceptor {
                   uri: 'spotify:playlist:3nl9GNcLS5xW883RgPypFL',
                 },
               ],
+            },
+            status: 200,
+          })
+        );
+        observer.complete();
+      });
+    }
+
+    if (
+      req.url.endsWith('playlists/50Gsv3p7qLLPVzfPBu8UcO') &&
+      req.method == 'GET'
+    ) {
+      return new Observable((observer) => {
+        observer.next(
+          new HttpResponse<Playlist>({
+            body: {
+              id: '50Gsv3p7qLLPVzfPBu8UcO',
+              href: 'https://api.spotify.com/v1/playlists/50Gsv3p7qLLPVzfPBu8UcO?locale=en-US%2Cen%3Bq%3D0.9%2Cpl-PL%3Bq%3D0.8%2Cpl%3Bq%3D0.7',
+              images: [
+                {
+                  height: 640,
+                  url: 'https://mosaic.scdn.co/640/ab67616d00001e02372cd9cefda2a6d3d11428fcab67616d00001e024246e3158421f5abb75abc4fab67616d00001e025ebcbbc6940d285ccd96e963ab67616d00001e02d1410c1372fab1e516328fa8',
+                  width: 640,
+                },
+                {
+                  height: 300,
+                  url: 'https://mosaic.scdn.co/300/ab67616d00001e02372cd9cefda2a6d3d11428fcab67616d00001e024246e3158421f5abb75abc4fab67616d00001e025ebcbbc6940d285ccd96e963ab67616d00001e02d1410c1372fab1e516328fa8',
+                  width: 300,
+                },
+                {
+                  height: 60,
+                  url: 'https://mosaic.scdn.co/60/ab67616d00001e02372cd9cefda2a6d3d11428fcab67616d00001e024246e3158421f5abb75abc4fab67616d00001e025ebcbbc6940d285ccd96e963ab67616d00001e02d1410c1372fab1e516328fa8',
+                  width: 60,
+                },
+              ],
+              name: 'Dua Lipa 🍑💫🔥',
+              tracks: {
+                href: 'https://api.spotify.com/v1/playlists/50Gsv3p7qLLPVzfPBu8UcO/tracks?offset=0&limit=100&locale=en-US%2Cen%3Bq%3D0.9%2Cpl-PL%3Bq%3D0.8%2Cpl%3Bq%3D0.7',
+                items: [
+                  {
+                    added_at: '2021-11-25T07:44:16Z',
+                    track: {
+                      album: {
+                        images: [
+                          {
+                            url: 'https://i.scdn.co/image/ab67616d0000b273d1410c1372fab1e516328fa8',
+                            width: 640,
+                            height: 640,
+                          },
+                          {
+                            url: 'https://i.scdn.co/image/ab67616d00001e02d1410c1372fab1e516328fa8',
+                            width: 300,
+                            height: 300,
+                          },
+                          {
+                            url: 'https://i.scdn.co/image/ab67616d00004851d1410c1372fab1e516328fa8',
+                            width: 64,
+                            height: 64,
+                          },
+                        ],
+                        name: 'Everyday Is Christmas (Deluxe Edition)',
+                      },
+                      artists: [
+                        {
+                          id: '5WUlDfRSoLAfcVSX1WnrxN',
+                          name: 'Sia',
+                          uri: 'spotify:artist:5WUlDfRSoLAfcVSX1WnrxN',
+                        },
+                      ],
+                      duration_ms: 206539,
+
+                      id: '1N1ZpYUJc9fwrqk53FGgWv',
+                      name: "Santa's Coming for Us",
+                      uri: 'spotify:track:1N1ZpYUJc9fwrqk53FGgWv',
+                    },
+                  },
+                ],
+                limit: 100,
+                offset: 0,
+                total: 1,
+                next: '',
+                previous: '',
+              },
+              type: 'playlist',
+              uri: 'spotify:playlist:50Gsv3p7qLLPVzfPBu8UcO',
             },
             status: 200,
           })
