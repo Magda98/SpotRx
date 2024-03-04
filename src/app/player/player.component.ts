7e2c6c25aa150f1c6698adb9103f9802700851c6
Magdalena Bober <33430525+Magda98@users.noreply.github.com>
2024-03-04 08:56:52 +0000

    detele unused imports

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,13 +1,17 @@
 import { PlayerService } from './../services/player.service';
-import { Component, OnDestroy, OnInit, computed, inject, signal } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 import { MatSliderDragEvent, MatSliderModule } from '@angular/material/slider';
 import {
-  EMPTY,
   Subscription,
   distinctUntilChanged,
-
   filter,
-
   firstValueFrom,
   map,
   tap,
@@ -18,7 +22,10 @@ import { MatIcon } from '@angular/material/icon';
 import { IconComponent } from '../icon/icon.component';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { injectMutation, injectQuery } from '@tanstack/angular-query-experimental';
+import {
+  injectMutation,
+  injectQuery,
+} from '@tanstack/angular-query-experimental';
 
 @Component({
   selector: 'app-player',
@@ -34,41 +41,44 @@ import { injectMutation, injectQuery } from '@tanstack/angular-query-experimenta
   ],
 })
 export class PlayerComponent implements OnInit, OnDestroy {
-  public playerService = inject(PlayerService)
-  private trackService = inject(TrackService)
-  private snackBarService = inject(MatSnackBar)
+  public playerService = inject(PlayerService);
+  private trackService = inject(TrackService);
+  private snackBarService = inject(MatSnackBar);
   public playerState$ = this.playerService.getPlayerState();
   public player?: Spotify.PlaybackState;
   private subscription = new Subscription();
-  private trackId = signal<string | null>(null)
+  private trackId = signal<string | null>(null);
   private isSavedTrackQuery = injectQuery(() => {
-    const trackId = this.trackId()
-    return this.trackService.checkUserSavedTracks(trackId ? [trackId] : [])
-  })
-  public isSavedTrack = computed(()=> this.isSavedTrackQuery.data()?.[0])
-  private addToFav = injectMutation((client)=> ({
-    mutationFn: (trackId: string) => firstValueFrom(this.trackService.saveTrack(trackId)),
-    onSuccess: ()=> {
+    const trackId = this.trackId();
+    return this.trackService.checkUserSavedTracks(trackId ? [trackId] : []);
+  });
+  public isSavedTrack = computed(() => this.isSavedTrackQuery.data()?.[0]);
+  private addToFav = injectMutation((client) => ({
+    mutationFn: (trackId: string) =>
+      firstValueFrom(this.trackService.saveTrack(trackId)),
+    onSuccess: () => {
       this.snackBarService.open('Added to Liked Songs.');
-      client.invalidateQueries({ queryKey: ['isSavedTrack'] })}
-  }))
-  private deleteFromFav = injectMutation((client)=> ({
-    mutationFn: (trackId: string) => firstValueFrom(this.trackService.deleteTrack(trackId)),
-    onSuccess: ()=> {
+      client.invalidateQueries({ queryKey: ['isSavedTrack'] });
+    },
+  }));
+  private deleteFromFav = injectMutation((client) => ({
+    mutationFn: (trackId: string) =>
+      firstValueFrom(this.trackService.deleteTrack(trackId)),
+    onSuccess: () => {
       this.snackBarService.open('Removed form Liked Songs.');
-      client.invalidateQueries({ queryKey: ['isSavedTrack'] })}
-  }))
-  private shuffle = injectMutation(()=> ({
-    mutationFn: (state: boolean) => firstValueFrom(this.trackService.toggleShuffle(state))
-  }))
+      client.invalidateQueries({ queryKey: ['isSavedTrack'] });
+    },
+  }));
+  private shuffle = injectMutation(() => ({
+    mutationFn: (state: boolean) =>
+      firstValueFrom(this.trackService.toggleShuffle(state)),
+  }));
 
   ngOnInit(): void {
-    this.subscription.add(
-      this.palyerStateChange$().subscribe()
-    );
+    this.subscription.add(this.palyerStateChange$().subscribe());
   }
 
-  private palyerStateChange$(){
+  private palyerStateChange$() {
     return this.playerState$.pipe(
       tap((player) => {
         this.player = player;
@@ -77,16 +87,16 @@ export class PlayerComponent implements OnInit, OnDestroy {
       distinctUntilChanged(),
       filter((id) => !!id),
       tap((id) => {
-       this.trackId.set(id);
+        this.trackId.set(id);
       })
-    )
+    );
   }
 
   toggleSavedTrack() {
-    const trackId = this.trackId()
+    const trackId = this.trackId();
     if (!trackId) return;
-    if (this.isSavedTrack()) return this.deleteFromFav.mutate(trackId)
-    return this.addToFav.mutate(trackId)
+    if (this.isSavedTrack()) return this.deleteFromFav.mutate(trackId);
+    return this.addToFav.mutate(trackId);
   }
 
   setVolume(volumeSlider: MatSliderDragEvent) {
@@ -100,7 +110,7 @@ export class PlayerComponent implements OnInit, OnDestroy {
   toggleShuffle() {
     if (!this.player) return;
     this.player.shuffle = !this.player.shuffle;
-    this.shuffle.mutate(this.player.shuffle)
+    this.shuffle.mutate(this.player.shuffle);
   }
 
   ngOnDestroy(): void {
