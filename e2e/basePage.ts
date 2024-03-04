9fa5bedd04625f4b8ee6dc817309a61ee17c3432
Magdalena Bober <33430525+Magda98@users.noreply.github.com>
2024-03-04 19:09:08 +0000

    setup playwright tests

---
+++ e2e/basePage.ts
@@ -0,0 +1,14 @@
+import { Page } from '@playwright/test';
+
+export class BasePage {
+  constructor(public readonly page: Page) {}
+
+  async waitToBeLoaded() {
+    const progressbars = await this.page.getByRole('progressbar').all();
+    await Promise.all(
+      progressbars.map((progressbar) =>
+        progressbar.waitFor({ state: 'detached' })
+      )
+    );
+  }
+}
--- e2e/setup.ts
+++ e2e/setup.ts
@@ -1,13 +1,15 @@
 import { test as base } from '@playwright/test';
 import { AUTH_DATA } from './authData';
+import { BasePage } from './basePage';
 
-export const test = base.extend({
-  page: async ({ page }, use) => {
+export const test = base.extend<{ basePage: BasePage }>({
+  basePage: async ({ page }, use) => {
+    const basePage = new BasePage(page);
     await page.goto('/');
     await page.evaluate((authData) => {
       localStorage.setItem('authData', authData);
     }, AUTH_DATA);
     await page.reload();
-    await use(page);
+    await use(basePage);
   },
 });
