5fd4eb9811a1a8c98e6a751862bf9d79b92a8ca0
Magda98 <magdakochman7@gmail.com>
2024-03-05 09:12:11 +0100

    home tests

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -5,6 +5,18 @@ test('home', async ({ basePage }) => {
   const playlistTitle = (await playlistLink.textContent()) ?? '';
   await playlistLink.click();
   await basePage.waitToBeLoaded();
-
   await basePage.page.getByRole('heading', { name: playlistTitle }).waitFor();
+});
+
+test('play playlist track', async ({ basePage }) => {
+  const playlistLink = basePage.page.getByRole('main').getByRole('link').nth(0);
+  const playlistTitle = (await playlistLink.textContent()) ?? '';
+  await playlistLink.click();
+  await basePage.waitToBeLoaded();
+  await basePage.page.getByRole('heading', { name: playlistTitle }).waitFor();
+
+  const track = basePage.page
+    .getByRole('main')
+    .getByRole('button', { name: 'HERO HERO HERO KizoBletka 2:' });
+  await track.click();
 });
