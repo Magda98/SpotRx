0f976ee9f6d7e5b5d5e1a16384813cb7cdab883e
Magda98 <magdakochman7@gmail.com>
2024-03-05 09:12:22 +0100

    saved tracks tests

---
+++ e2e/savedTracks.spec.ts
@@ -0,0 +1,11 @@
+import { test } from './setup';
+
+test('brwose saved tracks', async ({ basePage }) => {
+  const savedButton = basePage.page.getByRole('link', { name: 'Saved' });
+  await savedButton.click();
+  await basePage.page.waitForURL('/saved');
+  basePage.waitToBeLoaded();
+  await basePage.page.getByLabel('Next page').click();
+  basePage.waitToBeLoaded();
+  await basePage.page.screenshot({ path: 'screenshot.png' });
+});
