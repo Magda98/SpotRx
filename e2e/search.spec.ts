644bb856e2c42685ffeabe60ac16eaf2ce6f3c86
Magda98 <magdakochman7@gmail.com>
2024-03-05 09:12:31 +0100

    search tests

---
+++ e2e/search.spec.ts
@@ -0,0 +1,9 @@
+import { test } from './setup';
+
+test('search song', async ({ basePage }) => {
+  await basePage.page.getByRole('link', { name: 'Search' }).click();
+  await basePage.page.waitForURL('/search');
+  const searchInput = basePage.page.getByRole('textbox', { name: 'search' });
+  await searchInput.fill('Maryla');
+  await basePage.waitToBeLoaded();
+});
