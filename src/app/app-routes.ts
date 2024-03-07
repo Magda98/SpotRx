ee5957a878e3166c8925cc79849c970984756cc8
Magdalena Bober <33430525+Magda98@users.noreply.github.com>
2024-03-07 07:46:17 +0000

    add top tracks to navbar

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -3,10 +3,12 @@ import { PlaylistComponent } from './playlist/playlist.component';
 import { SavedTracksComponent } from './saved-tracks/saved-tracks.component';
 import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
+import { TopTracksComponent } from './top-tracks/top-tracks.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'saved', component: SavedTracksComponent },
   { path: 'search', component: SearchTracksComponent },
+  { path: 'top-tracks', component: TopTracksComponent },
   { path: 'playlist/:id', component: PlaylistComponent },
 ];
