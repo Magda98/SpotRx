4458e81116c9d5bc6f30dbec35b357c8c2be2715
Magda98 <magdakochman7@gmail.com>
2024-03-11 21:12:00 +0100

    add ripple

--- src/app/shared/button/button.directive.ts
+++ src/app/shared/button/button.directive.ts
@@ -5,7 +5,6 @@ import {
   HostListener,
   OnInit,
   Renderer2,
-  inject,
   input,
 } from '@angular/core';
 
@@ -17,6 +16,7 @@ export type ButtonMode = 'outline' | 'opacity';
 })
 export class ButtonDirective implements OnInit {
   mode = input<ButtonMode>();
+  rippleSpan?: HTMLElement;
   protected _elementClass: string[] = ['btn'];
 
   @HostBinding('class')
@@ -32,5 +32,43 @@ export class ButtonDirective implements OnInit {
     const buttonMode = this.mode();
     if (buttonMode)
       this._elementClass = [...this._elementClass, `btn-${buttonMode}`];
+
+    const rippleSpan = this.renderer.createElement('span');
+    rippleSpan.classList.add('ripple');
+    this.renderer.appendChild(this.el.nativeElement, rippleSpan);
+    this.rippleSpan = rippleSpan;
   }
+
+  @HostListener('click', ['$event'])
+  onClick(event: MouseEvent) {
+    const btn = this.el.nativeElement as HTMLElement;
+    const diameter = Math.max(btn.offsetWidth, btn.offsetHeight);
+    const x = event.clientX - (btn.offsetLeft + diameter / 2);
+    const y = event.clientY - (btn.offsetTop + diameter / 2);
+
+    if (this.rippleSpan) {
+      this.rippleSpan.style.width =
+        this.rippleSpan.style.height = `${diameter}px`;
+      this.rippleSpan.style.left = `${x}px`;
+      this.rippleSpan.style.top = `${y}px`;
+    }
+
+    const animation = this.rippleSpan?.animate(
+      [
+        {
+          transform: 'scale(0)',
+          opacity: 1,
+        },
+        {
+          transform: 'scale(2.5)',
+          opacity: 0,
+        },
+      ],
+      300
+    );
+
+    animation?.play();
+  }
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 }
