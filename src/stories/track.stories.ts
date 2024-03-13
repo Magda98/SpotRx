ea7f89593215b256f75b8b09fab701ed8bb5a7f3
Magdalena Bober <33430525+Magda98@users.noreply.github.com>
2024-03-13 10:53:09 +0000

    add track story

---
+++ src/stories/track.stories.ts
@@ -0,0 +1,25 @@
+import { argsToTemplate, type Meta, type StoryObj } from '@storybook/angular';
+import { TrackComponent } from 'src/app/shared/track/track.component';
+import { savedTracks } from 'src/tests/mocks';
+
+const meta: Meta<TrackComponent> = {
+  title: 'Example/Track',
+  component: TrackComponent,
+  tags: ['autodocs'],
+  argTypes: {},
+  render: (args) => ({
+    props: {
+      ...args,
+    },
+    template: `<app-track ${argsToTemplate(args)}></app-track>`,
+  }),
+};
+
+export default meta;
+type Story = StoryObj<TrackComponent>;
+
+export const Track: Story = {
+  args: {
+    track: savedTracks.items[0].track,
+  },
+};
