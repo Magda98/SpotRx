624603c496c7c9723e6bdd66facf43d556c7f48d
Magdalena Bober <33430525+Magda98@users.noreply.github.com>
2024-03-13 07:17:31 +0000

    add track component

---
+++ src/app/shared/track/track.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { Track } from '../interfaces/track';
+import { DurationPipe } from '../duration/duration.pipe';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+
+@Component({
+  selector: 'app-track',
+  standalone: true,
+  imports: [
+    CommonModule,
+    DurationPipe,
+    NgOptimizedImage,
+    NgxSkeletonLoaderModule,
+  ],
+  templateUrl: './track.component.html',
+  styleUrl: './track.component.scss',
+})
+export class TrackComponent {
+  track = input<Track>();
+  isLoading = input<Boolean>(false);
+}
