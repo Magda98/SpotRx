aaf33de086ca335d6080d6d00fc37e51f1b4bf61
Magdalena Bober <33430525+Magda98@users.noreply.github.com>
2024-03-19 07:24:18 +0000

    add icon stories

--- src/app/shared/icon/icon.component.ts
+++ src/app/shared/icon/icon.component.ts
@@ -2,26 +2,31 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Icons, icons } from '../../utils/icons';
 import {
   Component,
-  OnChanges,
   ViewEncapsulation,
+  effect,
   inject,
   input,
 } from '@angular/core';
 
 @Component({
   selector: 'app-icon',
-  template: `<span class="icon" [outerHTML]="icon"></span>`,
+  template: `@if (icon) {
+    <span class="icon" [outerHTML]="icon"></span>
+  }`,
   styleUrls: ['./icon.component.scss'],
   host: { class: 'app-icon' },
   encapsulation: ViewEncapsulation.None,
   standalone: true,
 })
-export class IconComponent implements OnChanges {
+export class IconComponent {
   private sanitizer = inject(DomSanitizer);
   type = input.required<Icons>();
-  icon!: SafeHtml;
+  icon?: SafeHtml;
 
-  ngOnChanges(): void {
-    this.icon = this.sanitizer.bypassSecurityTrustHtml(icons[this.type()]);
+  constructor() {
+    effect(() => {
+      const type = this.type();
+      if (type) this.icon = this.sanitizer.bypassSecurityTrustHtml(icons[type]);
+    });
   }
 }
---
+++ src/stories/icon.stories.ts
@@ -0,0 +1,31 @@
+import { argsToTemplate, type Meta, type StoryObj } from '@storybook/angular';
+import { IconComponent } from 'src/app/shared/icon/icon.component';
+import { Icons } from 'src/app/utils/icons';
+
+const meta: Meta<IconComponent> = {
+  title: 'Example/Icon',
+  component: IconComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      description: 'icons',
+      options: ['heart', 'heartOutline', 'logo', 'menu', 'shuffle'] as Icons[],
+      control: { type: 'select' },
+    },
+  },
+  render: (args) => ({
+    props: {
+      ...args,
+    },
+    template: `<app-icon ${argsToTemplate(args)}></app-icon>`,
+  }),
+};
+
+export default meta;
+type Story = StoryObj<IconComponent>;
+
+export const Icon: Story = {
+  args: {
+    type: 'heart',
+  },
+};
