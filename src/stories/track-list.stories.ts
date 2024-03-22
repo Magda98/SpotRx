fb5dec21cbf7bbb6c21263aac987abc543893779
Magdalena Bober <33430525+Magda98@users.noreply.github.com>
2024-03-22 11:46:18 +0000

    add track-list story

---
+++ src/stories/track-list.stories.ts
@@ -0,0 +1,40 @@
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { argsToTemplate, moduleMetadata, applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { PlayerService } from 'src/app/shared/services/player.service';
+import { TrackListComponent } from 'src/app/shared/track-list/track-list.component';
+import { savedTracks } from 'src/tests/mocks';
+
+const meta: Meta<TrackListComponent> = {
+  title: 'Example/Track List',
+  component: TrackListComponent,
+  tags: ['autodocs'],
+  argTypes: {},
+  decorators: [
+    applicationConfig({
+      providers: [
+          importProvidersFrom(HttpClientModule)
+      ],
+  }),
+    moduleMetadata({
+      providers: [PlayerService, ]
+    })
+  ],
+  render: (args) => ({
+    props: {
+      ...args,
+    },
+    template: `<app-track-list ${argsToTemplate(args)}></app-track-list>`,
+  }),
+};
+
+export default meta;
+type Story = StoryObj<TrackListComponent>;
+
+export const Track: Story = {
+  args: {
+    tracksList: savedTracks,
+    isLoading: false,
+    total: savedTracks.total
+  },
+};
